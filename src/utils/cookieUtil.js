bcd6cda51c18f206d356f9ec19c3987d150b54f8
WyvonJ <wyvonj@gmail.com>
2017-05-25 20:31:24 +0800

    daily

--- src/utils/cookieUtil.js
+++ src/utils/cookieUtil.js
@@ -1,9 +1,12 @@
 let Cookie = {
   get: (name) => {
-    let arr, reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+    let arr
+    let reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
     if (arr = document.cookie.match(reg)) {
-      return unescape(decodeURIComponent(arr[2])) } else {
-      return null }
+      return unescape(decodeURIComponent(arr[2]))
+    } else {
+      return null
+    }
   },
   set: (name, value, expires, path, domain, secure) => {
     let cookieText = encodeURIComponent(name) + '=' + encodeURIComponent(value)
